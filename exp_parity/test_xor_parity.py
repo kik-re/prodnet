5d59c69c89f1a8c4756e6e1c79637c4c6d7d8839
slavo <slavomirholenda@gmail.com>
2023-04-18 18:43:24 +0200

    implemented the save function

--- exp_parity/test_xor_parity.py
+++ exp_parity/test_xor_parity.py
@@ -2,15 +2,16 @@ from statistics import mean, stdev
 from base.data_util import parity_minus
 from base.net_util import Tanh, QuasiPow
 from base.exp_convergence import ExpConvergence
-from base.util import humanreadible_runtime
+from base.util import humanreadible_runtime, save_results
 
-p = 7
+p = 2
 if p == 2:
-    expname = 'xor_hidden'
+    expname = 'xor'
 inputs, labels = parity_minus(p)
-expname = 'parity{}_hidden'.format(p)
+expname = 'parity_{}_hidden'.format(p)
 
-hidden_size = [150,170,200]
+
+hidden_size = [3,4,5]
 exp_params = {
     "repetitions": 100,
     "net_hyperparams": {
@@ -39,4 +40,4 @@ for h in hidden_size:
     plot_expnet_nets.append("{} {}\n".format(h, sum(results_converge)))
     plot_expnet_epcs.append("{} {} {}\n".format(h, mean(results_epochs), stdev(results_epochs)))
 
-# save_results("mulnet", expname, plot_expnet_nets, plot_expnet_epcs)
+save_results("mulnet", expname, plot_expnet_nets, plot_expnet_epcs)
